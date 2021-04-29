c2fa89a75e54c54ee9c85fabe330a8cd005d0823
thiagobcav <83366548+thiagobcav@users.noreply.github.com>
2021-04-28 22:05:07 -0300

    Update lacserver.sh

--- lacserver.sh
+++ lacserver.sh
@@ -10,10 +10,10 @@ echo 'Baixando arquivos necessários'
 sudo wget https://www.dropbox.com/s/83w5p2nwr7m4bp1/LAC_Linux_v1.5.8.zip?dl=1 -O LAC_Linux_v.1.5.8.zip
 
 echo 'Descompactando Arquivos'
-sudo unzip LAC_Linux_v1.5.8.zip;
+sudo unzip LAC_Linux_v.1.5.8.zip;
 
 echo 'Ativando root'
-sudo chmod +x LAC_Linux_v1.5.8.x86_64;
+sudo chmod +x LAC_Linux_v.1.5.8.x86_64;
 
 echo 'Criando diretórios'
 sudo mkdir .config; cd .config; mkdir unity3d; cd unity3d; mkdir MA; cd MA; mkdir LAC; mkdir editor; cd;
@@ -22,7 +22,7 @@ echo 'Criando Configurações do servidor'
 sudo mv ServerConfig.txt /root/.config/unity3d/MA/LAC;
 
 echo 'Editando Configurações do servidor'
-sudo cd; rm LAC_Linux_v1.5.8.zip;
+sudo cd; rm LAC_Linux_v.1.5.8.zip;
 
 echo 'Criando Configurações do servidor'
 sudo cd /root/.config/unity3d/MA/LAC; nano ServerConfig.txt &&
